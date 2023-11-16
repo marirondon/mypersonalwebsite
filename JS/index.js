a5085fe066fe7444edc63aa8fafbb4bb555de443
marirondon <137960158+marirondon@users.noreply.github.com>
2023-11-16 16:16:21 -0500

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+const navLinkEls = document.querySelectorAll ('.nav.link');
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+    if (navLinkEl.href.includes(windowPathname)) {
+        navLinkEl.classList.add('active');
+    }
+});
